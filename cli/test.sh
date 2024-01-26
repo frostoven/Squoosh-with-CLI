ebdbf83290c972d91853486f36fd43a5bbc876f0
aggregate1166877 <noreply@frostoven.com>
2024-01-26 15:14:48 +1300

    #5 Add basic test based on problem description.

--- cli/test.sh
+++ cli/test.sh
@@ -27,6 +27,13 @@ npx @frostoven/squoosh-cli --avif '{}' /tmp/squoosh-test/
 npx @frostoven/squoosh-cli --avif '{"cqLevel":33,"cqAlphaLevel":-1,"denoiseLevel":0,"tileColsLog2":0,"tileRowsLog2":0,"speed":6,"subsample":1,"chromaDeltaQ":false,"sharpness":0,"tune":0}' /tmp/squoosh-test/
 
 # =============================================================================
+# Test resize and output options
+# =============================================================================
+
+# Test for issue #5
+squoosh-cli --resize '{"enabled":true,"width":1080,"height":1624,"method":"lanczos3","fitMethod":"contain","premultiply":true,"linearRGB":true}' --webp '{"quality":75,"target_size":0,"target_PSNR":0,"method":6,"sns_strength":50,"filter_strength":60,"filter_sharpness":0,"filter_type":1,"partitions":0,"segments":4,"pass":1,"show_compressed":0,"preprocessing":0,"autofilter":0,"partition_limit":0,"alpha_compression":1,"alpha_filtering":1,"alpha_quality":100,"lossless":0,"exact":0,"image_hint":0,"emulate_jpeg_size":0,"thread_level":0,"low_memory":0,"near_lossless":100,"use_delta_palette":0,"use_sharp_yuv":0}' -d /tmp/squoosh-test/out /tmp/squoosh-test
+
+# =============================================================================
 # Test preprocessor options
 # =============================================================================
 
